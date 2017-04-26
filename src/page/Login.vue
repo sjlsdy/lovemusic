3b9c2a9e2d6aa7bb4a29d2ea4f4f23f898e2b5e6
SIM <sim@SIMdeiMac.local>
2017-04-26 23:51:52 +0800

    import mint-ui

---
+++ src/page/Login.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<mt-picker :slots="slots" @change="onValuesChange"></mt-picker>
+	</div>
+</template>
+
+<script>
+	export default {
+		methods: {
+			onValuesChange(picker, values) {
+				if(values[0] > values[1]) {
+					console.log(this)
+					picker.setSlotValue(1, values[0]);
+				}
+			}
+		},
+		data() {
+			return {
+				slots: [{
+					flex: 1,
+					values: ['2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06'],
+					className: 'slot1',
+					textAlign: 'right'
+				}, {
+					divider: true,
+					content: '-',
+					className: 'slot2'
+				}, {
+					flex: 1,
+					values: ['2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06'],
+					className: 'slot3',
+					textAlign: 'left'
+				}]
+			};
+		}
+	};
+</script>
+
+<style>
+
+</style>
